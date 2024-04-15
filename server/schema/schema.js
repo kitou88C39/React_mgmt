3c883c5615a322ff72d6d30018165b87caa09674
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-15 21:23:23 +0900

    server/schema/schema.jsを編集する

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,8 +17,12 @@ const ProjectType = new GraphQLObjectType({
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     status: { type: GraphQLString },
-    client: { type: ClientType },
-    resolve(parent, args) {},
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.client.Id);
+      },
+    },
   }),
 });
 
