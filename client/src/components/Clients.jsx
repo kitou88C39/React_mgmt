c493433095c9c48bc0363964cbba41aca9360439
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-18 21:02:39 +0900

    client/src/App.jsを編集する

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,11 +1,17 @@
 import { gql, useQuery } from '@apollo/client';
 
 const GET_CLIENTS = gql`
-query getClients {
-    
-}
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
 `;
 
 export default function Clients() {
+  const { loading } = useQuery(GET_CLIENTS);
   return <div>Clients</div>;
 }
