3623ec95e1b91b4b09bc8c6c7eba440728639764
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-18 20:41:39 +0900

    client/src/App.jsを編集する

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <>
