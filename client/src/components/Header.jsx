4422bf1bee7985c21653e6435062e8e5d9b7dd9d
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-18 10:48:57 +0900

    client/src/compomentsにHeader.jsxを作成する

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import logo from './assets/logo.png';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className='navbar bg-light mb-4 p-4'>
+      <div className='container'>Header</div>
+    </nav>
+  );
 }
