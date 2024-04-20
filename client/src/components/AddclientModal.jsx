96ab97495a5c9b7a1676f2d3e032777b8e6d4792
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-20 09:52:07 +0900

    client/srcに、mutationsフォルダとAddclientModal.jsxを作成する

--- client/src/components/AddclientModal.jsx
+++ client/src/components/AddclientModal.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
 
 export default function AddclientModal() {
   return <div>AddclientModal</div>;
