c65dcd4a992f66dc59d15d8902e84889aab37ca0
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-20 17:04:50 +0900

    client/srcに、mutationsフォルダとclientMutations.jsを作成する

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,5 +1,10 @@
 import { gql } from '@apollo/client';
 
+const ADD_CLIENT = gql`
+  mutation AddClient($name: String!, $email: String!, $company: String!){
+}
+`;
+
 const DELETE_CLIENTS = gql`
     mutation deleteClient($id: ID!){
         deleteClient($id: $id){
