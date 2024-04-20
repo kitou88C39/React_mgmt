ff997c8f1fd21d4de9b06d9677091b0bdb9b05ce
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-20 21:11:49 +0900

    client/src/compomentsにProjects.jsxを作成する

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -4,19 +4,21 @@ import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function Projects() {
   const { loading, error, data } = useQuery(GET_PROJECTS);
+
   if (loading) return <Spinner />;
   if (error) return <p>Something Went Wrong</p>;
 
   return (
-  <>
-    {data.projects.length > 0 ? (
-      <div className='row'>
-        {data.projects.map((project) => (
-              <ProjectCard key={project.id} project={project} />
-      </div>
-    ) : (
-      <p>NO PROJECTS</p>
-    )}
-  </>;
-  )
+    <>
+      {data.projects.length > 0 ? (
+        <div className='row'>
+          {data.projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      ) : (
+        <p>NO PROJECTS</p>
+      )}
+    </>
+  );
 }
