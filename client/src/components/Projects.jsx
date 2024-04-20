bdc961fed0b43ca368dd7c3211851e7406ae2312
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-20 21:13:00 +0900

    client/src/compomentsにProjects.jsxを作成する

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import Spinner from 'Spinner';
 import { useQuery } from '@apollo/client';
 import { GET_PROJECTS } from '../queries/projectQueries';
+import ProjectCard from './ProjectCard';
 
 export default function Projects() {
   const { loading, error, data } = useQuery(GET_PROJECTS);
