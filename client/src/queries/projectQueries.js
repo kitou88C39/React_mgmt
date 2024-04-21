c555f3a7faddc7cf907417f6ab3252be9fef74f1
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 16:11:57 +0900

    queriesにprojectQueries.jsを作成する

--- client/src/queries/projectQueries.js
+++ client/src/queries/projectQueries.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 const GET_PROJECTS = gql`
   query getProjects {
-    projectss {
+    projects {
       id
       name
       status
@@ -10,4 +10,21 @@ const GET_PROJECTS = gql`
   }
 `;
 
-export { GET_PROJECTS };
+const GET_PROJECT = gql`
+  query getProject($id: ID!) {
+    project(id: $id) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { GET_PROJECTS, GET_PROJECT };
