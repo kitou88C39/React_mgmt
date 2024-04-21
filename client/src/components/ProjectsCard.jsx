e21cd38f679accc3097393c85340f3220f8cd9d0
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 11:08:24 +0900

    client/src/compomentsにProjectCard.jsxを作成する

--- client/src/components/ProjectsCard.jsx
+++ client/src/components/ProjectsCard.jsx
@@ -1,6 +1,6 @@
 export default function ProjectsCard({ project }) {
   return (
-    <div className='col-md-4'>
+    <div className='col-md-6'>
       <div className='card mb-3'>
         <div className='card-body'>
           <div className='d-flex justify-content-between-align-items-center'>
@@ -9,6 +9,9 @@ export default function ProjectsCard({ project }) {
               View
             </a>
           </div>
+          <p className='small'>
+            Status:<strong>{project.status}</strong>
+          </p>
         </div>
       </div>
     </div>
