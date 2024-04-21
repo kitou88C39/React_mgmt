75eb6dc5aa3087e6b9523c61e3c99c5f0b7c64b3
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 16:17:30 +0900

    client/src/compomentsにpagesフォルダとProject.jsxを作成する

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,4 +1,7 @@
 import { Link, useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import { useQuery } from '@apollo/client';
+import { GET_PROJECT } from '../queries/projectQueries';
 
 export default function Project() {
   return <div>Project</div>;
