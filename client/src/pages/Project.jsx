cb403a53a1241bcb2c5869f839ddf03585cdfac7
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 17:06:21 +0900

    client/src/compomentsにpagesフォルダとProject.jsxを作成する

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -21,6 +21,7 @@ export default function Project() {
           <p>{data.project.description}</p>
           <h5 className='mt-3'>Project Status</h5>
           <p className='lead'>{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
         </div>
       )}
     </>
