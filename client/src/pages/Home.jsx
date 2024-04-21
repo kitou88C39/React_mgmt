5429b5cdaa7d852d2c8c5b0a3e431d149fab89bf
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 15:14:28 +0900

    client/src/compomentsにpagesフォルダとHome.jsxを作成する

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import Clients from '../components/Clients';
+import Projects from '../components/Projects ';
+import AddclientModal from '../components/AddclientModal';
 
 export default function Home() {
   return (
@@ -6,8 +8,9 @@ export default function Home() {
       <div className='d-flex gap-3 mb-4'>
         <AddclientModal />
       </div>
-      <Clients />
       <Projects />
+      <hr />
+      <Clients />
     </>
   );
 }
