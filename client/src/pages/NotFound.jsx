c4a953d611ec0ab57000dcb43abe1ed08e79b876
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 15:28:13 +0900

    client/src/compomentsにpagesフォルダとNotFound.jsxを作成する

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,5 +2,5 @@ import { FaExclamationTriangle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return <div className='d-flex flex-column justify-content-center'></div>;
 }
