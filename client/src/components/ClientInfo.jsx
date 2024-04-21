6738197d3acebb87374dd4c7f0124085c181238a
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 19:59:52 +0900

    client/src/compomentsにClientInfo.jsxを作成する

--- client/src/components/ClientInfo.jsx
+++ client/src/components/ClientInfo.jsx
@@ -1,5 +1,25 @@
-import { FaEnvelope, FaPhone, FaId } from 'react-icons/fa';
+import { FaEnvelope, FaPhone, FaId, FaIdBadge } from 'react-icons/fa';
 
 export default function ClientInfo({ client }) {
-  return <div>ClientInfo</div>;
+  return (
+    <>
+      <h5 className='mt-5'>
+        Client Information
+        <ul className='list-group'>
+          <li className='list-group-item'>
+            <FaIdBadge className='icon' />
+            {client.name}
+          </li>
+          <li className='list-group-item'>
+            <FaIdBadge className='icon' />
+            {client.name}
+          </li>
+          <li className='list-group-item'>
+            <FaIdBadge className='icon' />
+            {client.name}
+          </li>
+        </ul>
+      </h5>
+    </>
+  );
 }
