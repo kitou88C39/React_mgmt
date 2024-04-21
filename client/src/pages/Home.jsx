07ef859cc2da23f7c9726e55f4b04f9802898a30
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 20:08:36 +0900

    client/srcに、mutationsフォルダとAddClientModal.jsxを作成する

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Clients from '../components/Clients';
 import Projects from '../components/Projects ';
-import AddclientModal from '../components/AddclientModal';
+import AddclientModal from '../components/AddClientModal';
 
 export default function Home() {
   return (
