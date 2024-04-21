f8e67aefcf2ed1dc52e4b4622dabeedd4e326e87
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-21 20:12:29 +0900

    client/srcに、mutationsフォルダとAddProjectModal.jsxを作成する

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,123 @@
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { ADD_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
+
+export default function AddclientModal() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({
+        query: GET_CLIENTS,
+      });
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === '' || email === '' || phone === '') {
+      return alert('Please fill in all fields');
+    }
+    addClient(name, email, phone);
+    setName('');
+    setEmail('');
+    setPhone('');
+  };
+
+  return (
+    <>
+      <button
+        type='button'
+        className='btn btn-secondary'
+        data-toggle='modal'
+        data-target='#addClientModal'
+      >
+        <div className='d-flex align-items-center'>
+          <FaUser className='icon' />
+          <div>Add Client</div>
+        </div>
+      </button>
+
+      <div
+        className='modal fade'
+        id='exampleModal'
+        aria-labelledby='AddClientModalLabel'
+        aria-hidden='true'
+      >
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title' id='AddClientModalLabel'>
+                Add Client
+              </h5>
+              <button
+                type='button'
+                className='close'
+                data-dismiss='modal'
+                aria-label='Close'
+              ></button>
+            </div>
+            <div className='modal-body'>
+              <from onSubmit={onSubmit}>
+                <div className='mb-3'>
+                  <label className='form-label'>
+                    Name
+                    <input
+                      type='name'
+                      className='from-control'
+                      id='name'
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className='mb-3'>
+                  <label className='form-label'>
+                    Email
+                    <input
+                      type='email'
+                      className='from-control'
+                      id='email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className='mb-3'>
+                  <label className='form-label'>
+                    Phone
+                    <input
+                      type='phone'
+                      className='from-control'
+                      id='phone'
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <button
+                  type='submit'
+                  data-bs-dismiss='modal'
+                  className='btn btn-secondary'
+                >
+                  Submit
+                </button>
+              </from>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
