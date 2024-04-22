74ad899ac4bdd6b4d061cb0a0903c2a6ada6a4f7
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-22 09:20:44 +0900

    client/src/mutationsにprojectMutations.jsを作成する

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,5 +1,9 @@
 import { gql } from '@apollo/client';
 
+const ADD_PROJECT = `
+  mutation AddProject($name: String!, $description: String!, $clientId: ID!) {
+`;
+
 export default function projectMutations() {
   return <div>projectMutations</div>;
 }
