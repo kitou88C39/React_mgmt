a1f01bbb33220044bfdf034775313e28d723f2d1
kitoumasaomi <kanfutrooper@gmail.com>
2024-04-22 15:52:08 +0900

    client/src/compomentsにDeleteProjectButton.jsxを作成する

--- client/src/components/DeleteProjectButton.jsx
+++ client/src/components/DeleteProjectButton.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
+import { GET_PROJECTS } from '../queries/projectQueries';
+import { useMutation } from '@apollo/client';
 
 export default function DeleteProjectButton() {
   return <div>DeleteProjectButton</div>;
